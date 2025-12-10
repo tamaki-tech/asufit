6debc5add710f668313e810b404e64336b26f7dd
tamaki_tech <nmatsumoto@matsumotonaokinoMacBook-Pro.local>
2025-12-10 23:48:27 +0900

    fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,8 +2,11 @@ import { client } from "$lib/api/client";
 import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ fetch }) => {
+  // Fetch data from the hono.js API
   const res = await client.book.$get({}, { fetch });
   const data = await res.json();
+
+  // Return the data to the page
   return {
     books: data,
   };
