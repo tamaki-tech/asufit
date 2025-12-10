8ffc467fd2ebb841519d20b874d200c6cb62a0d8
tamaki_tech <nmatsumoto@matsumotonaokinoMacBook-Pro.local>
2025-12-10 23:45:54 +0900

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="p-4">
-  <h1 class="text-3xl font-bold underline mb-4">asufit</h1>
+  <h1 class="text-3xl font-bold underline mb-4">Hello Sveltekit + hono.js</h1>
   <p class="mb-2">API Result:</p>
   <pre class="bg-gray-100 p-4 rounded">{JSON.stringify(
       data.books,
