784da5cce305899ab128df8e9ce90d4d1cd6b336
tamaki_tech <oshaburimegane@gmail.com>
2026-01-02 21:27:08 +0900

    refactor: 不要なrouteを削除

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,34 +1,7 @@
 import { Hono } from "hono";
-import { zValidator } from "@hono/zod-validator";
-import { z } from "zod";
 import { authRoute } from "./routes/auth";
 
-// sample routes
-const bookRoute = new Hono()
-  .get("/", (c) => {
-    const books = ["吾輩は猫である", "こころ", "人間失格", "銀河鉄道の夜"];
-    const randomBook = books[Math.floor(Math.random() * books.length)];
-    return c.json({ result: randomBook });
-  })
-  .get("/:id", (c) => c.json({ result: `get ${c.req.param("id")}` }))
-  .post(
-    "/",
-    zValidator(
-      "json",
-      z.object({
-        title: z.string().min(1),
-        author: z.string().min(1),
-      })
-    ),
-    async (c) => {
-      const body = c.req.valid("json");
-      return c.json({ result: "create a book", data: body }, 201);
-    }
-  );
-
 // main app
-export const app = new Hono()
-  .route("/book", bookRoute)
-  .route("/auth", authRoute);
+export const app = new Hono().route("/auth", authRoute);
 
 export type ApiRoute = typeof app;
